3853417d35390ba4b4ab90eb9376fb125fd73d51
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2022-06-07 03:49:30 +0300

    Add radio stack type values

--- frontend/src/pages/Device.vue
+++ frontend/src/pages/Device.vue
@@ -28,6 +28,10 @@
               <span>Radio FW:</span>
               <span>{{ info.radio_alive !== false ? info.radio_stack_major + '.' + info.radio_stack_minor + '.' + info.radio_stack_sub : 'corrupt' }}</span>
             </p>
+            <p>
+              <span>Radio stack:</span>
+              <span>{{ radioStackType }}</span>
+            </p>
           </div>
           <div class="column items-center">
             <h5>{{ info.hardware_name }}</h5>
@@ -98,6 +102,67 @@ export default defineComponent({
       }),
       screenScale: ref(1),
       channels: ref({})
+    }
+  },
+
+  computed: {
+    radioStackType () {
+      switch (parseInt(this.info.radio_stack_type)) {
+        case 0x01:
+          return 'full'
+        case 0x02:
+          return 'BLE_HCI'
+        case 0x03:
+          return 'light'
+        case 0x04:
+          return 'BLE_BEACON'
+        case 0x05:
+          return 'BLE_BASIC'
+        case 0x06:
+          return 'BLE_FULL_EXT_ADV'
+        case 0x07:
+          return 'BLE_HCI_EXT_ADV'
+        case 0x10:
+          return 'THREAD_FTD'
+        case 0x11:
+          return 'THREAD_MTD'
+        case 0x30:
+          return 'ZIGBEE_FFD'
+        case 0x31:
+          return 'ZIGBEE_RFD'
+        case 0x40:
+          return 'MAC'
+        case 0x50:
+          return 'BLE_THREAD_FTD_STATIC'
+        case 0x51:
+          return 'BLE_THREAD_FTD_DYAMIC'
+        case 0x60:
+          return '802154_LLD_TESTS'
+        case 0x61:
+          return '802154_PHY_VALID'
+        case 0x62:
+          return 'BLE_PHY_VALID'
+        case 0x63:
+          return 'BLE_LLD_TESTS'
+        case 0x64:
+          return 'BLE_RLV'
+        case 0x65:
+          return '802154_RLV'
+        case 0x70:
+          return 'BLE_ZIGBEE_FFD_STATIC'
+        case 0x71:
+          return 'BLE_ZIGBEE_RFD_STATIC'
+        case 0x78:
+          return 'BLE_ZIGBEE_FFD_DYNAMIC'
+        case 0x79:
+          return 'BLE_ZIGBEE_RFD_DYNAMIC'
+        case 0x80:
+          return 'RLV'
+        case 0x90:
+          return 'BLE_MAC_STATIC'
+        default:
+          return this.info.radio_stack_type
+      }
     }
   },
 
