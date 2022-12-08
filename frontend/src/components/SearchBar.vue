fe4835614157537ea84da241a3613de0d39b8652
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2022-12-08 04:49:57 +0300

    Add search bar

---
+++ frontend/src/components/SearchBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="full-width">
+    <q-input dense borderless v-model="text" class="full-width q-px-md bg-grey-3" style="border-radius: 20px" label="Search">
+      <template v-slot:prepend>
+        <q-icon name="mdi-magnify" class="text-grey-7"/>
+      </template>
+    </q-input>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'AppList',
+  props: {
+    apps: Array
+  },
+
+  setup () {
+    return {
+      text: ref('')
+    }
+  },
+
+  computed: {
+  },
+
+  methods: {
+  },
+
+  mounted () {
+  }
+})
+</script>
+
+<style lang="sass" scoped>
+input
+  color: black !important
+</style>
