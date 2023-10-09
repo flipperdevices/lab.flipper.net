299dde0ac3e93a12ed19c5442cef6141d74bf1cd
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2023-10-09 07:30:04 +0300

    feat: Refactor NFC Tools

--- frontend/src/util/mfkey32v2/mfkey.js
+++ frontend/src/util/mfkey32v2/mfkey.js
@@ -1,14 +1,15 @@
 import { Operation } from '../util'
 const operation = new Operation()
+const MFKEY_TIMEOUT = 15000
 let mfkey
 
 async function startMfkey (args) {
   mfkey = new Worker(new URL('./mfkey-worker.js', import.meta.url))
   const start = operation.create(mfkey, 'start', JSON.parse(JSON.stringify(args)))
   const timeout = setTimeout(() => {
-    operation.terminate({ status: 0, error: 'mfkey run killed on timeout' })
+    operation.terminate({ status: 0, error: 'killed on timeout' })
     mfkey.terminate()
-  }, 10000)
+  }, MFKEY_TIMEOUT)
   mfkey.onmessage = (e) => {
     if (e.data.operation === 'output') {
       clearTimeout(timeout)
