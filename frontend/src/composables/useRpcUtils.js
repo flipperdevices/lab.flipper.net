44d4c41dbc4dfa82a22c4863888e706e5a5d0283
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2023-12-26 01:02:08 +0300

    feat: Do not notify about RPC errors in UI

--- frontend/src/composables/useRpcUtils.js
+++ frontend/src/composables/useRpcUtils.js
@@ -1,12 +1,12 @@
-import showNotif from 'composables/useShowNotif'
+// import showNotif from 'composables/useShowNotif'
 import { log } from 'composables/useLog'
 
 const rpcErrorHandler = (componentName, error, command) => {
   error = error.toString()
-  showNotif({
+  /* showNotif({
     message: `RPC error in command '${command}': ${error}`,
     color: 'negative'
-  })
+  }) */
   log({
     level: 'error',
     message: `${componentName}: RPC error in command '${command}': ${error}`
