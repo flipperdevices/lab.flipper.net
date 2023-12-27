1c4b0d4ef281e4b4464bc8c0e464ceb7ae160d1d
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2023-12-27 20:13:32 +0300

    feat: CI: compile protobuf, move workers

---
+++ frontend/beforeBuild.js
@@ -0,0 +1,118 @@
+const { exec } = require('child_process')
+const fs = require('fs')
+const path = require('path')
+const readline = require('readline')
+
+const compileProtofiles = () => {
+  return new Promise((resolve, reject) => {
+    exec('echo "compile-protofiles START" && npx pbjs -t static-module -w es6 --no-comments --lint "eslint-disable block-scoped-var, id-length, no-control-regex, no-magic-numbers, no-prototype-builtins, no-redeclare, no-shadow, no-var, sort-vars, camelcase, default-case-last, no-mixed-operators" -o src/flipper-js/protobufCompiled.js ./flipperzero-protobuf/*.proto && npx eslint --fix src/flipper-js/protobufCompiled.js && echo "compile-protofiles END"', (err, stdout, stderr) => {
+      if (err) {
+        console.error(err)
+      }
+      console.log(stdout)
+      if (stderr) {
+        console.error(stderr)
+      }
+      resolve()
+    })
+  })
+}
+
+const walk = (dir, done) => {
+  let results = []
+  fs.readdir(dir, (err, list) => {
+    if (err) return done(err)
+    let i = 0;
+    (function next () {
+      let file = list[i++]
+      if (!file) return done(null, results)
+      file = path.resolve(dir, file)
+      fs.stat(file, (err, stat) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        if (stat && stat.isDirectory()) {
+          walk(file, (err, res) => {
+            if (err) {
+              console.error(err)
+              return
+            }
+            results = results.concat(res)
+            next()
+          })
+        } else {
+          results.push(file)
+          next()
+        }
+      })
+    })()
+  })
+}
+const walkPromise = (dir) => {
+  return new Promise((resolve, reject) => {
+    walk(dir, (err, result) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(result)
+    })
+  })
+}
+const copyPromise = (src, dest) => {
+  return new Promise((resolve, reject) => {
+    fs.copyFile(src, dest, (err) => {
+      if (err) {
+        reject(err)
+      }
+      resolve()
+    })
+  })
+}
+const getFirstLine = async (pathToFile) => {
+  const readable = fs.createReadStream(pathToFile)
+  const reader = readline.createInterface({ input: readable })
+  const line = await new Promise((resolve) => {
+    reader.on('line', (line) => {
+      reader.close()
+      resolve(line)
+    })
+  })
+  readable.close()
+  return line
+}
+
+const moveWorkers = async () => {
+  const result = await walkPromise(path.join(__dirname, 'src'))
+  const workersList = result.filter(e => e.endsWith('worker.js')).map(e => e.slice(e.indexOf('frontend/src') + 9))
+  const dependencies = new Set()
+
+  for (const workerPath of workersList) {
+    const workerDir = workerPath.slice(0, workerPath.lastIndexOf('/'))
+    const fileName = workerPath.slice(workerPath.lastIndexOf('/') + 1)
+    const dest = 'src-electron/extraResources/workers/' + fileName
+    const absolutePath = path.join(__dirname, workerPath)
+    const absoluteDest = path.join(__dirname, dest)
+
+    const firstLine = await getFirstLine(absolutePath)
+    if (firstLine.startsWith('// dependencies: ')) {
+      const workerDependencies = JSON.parse(firstLine.slice(17))
+      for (const workerDependency of workerDependencies) {
+        dependencies.add(path.join(workerDir, workerDependency))
+      }
+    }
+
+    await copyPromise(absolutePath, absoluteDest)
+  }
+
+  for (const dependency of dependencies) {
+    const fileName = dependency.slice(dependency.lastIndexOf('/') + 1)
+    const dest = 'src-electron/extraResources/workers/' + fileName
+    copyPromise(path.join(__dirname, dependency), path.join(__dirname, dest))
+  }
+}
+
+module.exports = {
+  compileProtofiles,
+  moveWorkers
+}
--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -1,54 +1,29 @@
-/*
- * This file runs in a Node context (it's NOT transpiled by Babel), so use only
- * the ES6 features that are supported by your Node version. https://node.green/
- */
-
-// Configuration for your app
-// https://quasar.dev/quasar-cli/quasar-conf-js
-
 /* eslint-env node */
 const ESLintPlugin = require('eslint-webpack-plugin')
 const { configure } = require('quasar/wrappers')
 const path = require('node:path')
 
+const { compileProtofiles, moveWorkers } = require('./beforeBuild.js')
+
 module.exports = configure(function (ctx) {
   return {
-    // https://quasar.dev/quasar-cli/supporting-ts
     supportTS: false,
-
-    // https://quasar.dev/quasar-cli/prefetch-feature
-    // preFetch: true,
-
-    // app boot file (/src/boot)
-    // --> boot files are part of "main.js"
-    // https://quasar.dev/quasar-cli/boot-files
     boot: [
       'axios'
     ],
 
-    // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: [
       'app.sass'
     ],
 
-    // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
-      // 'ionicons-v4',
-      // 'mdi-v5',
-      // 'fontawesome-v5',
-      // 'eva-icons',
-      // 'themify',
-      // 'line-awesome',
-      // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
-
-      'roboto-font', // optional, you are not bound to it
-      'material-icons', // optional, you are not bound to it
+      'roboto-font',
+      'material-icons',
       'mdi-v5'
     ],
 
-    // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'history', // available values: 'hash', 'history'
+      vueRouterMode: 'history',
 
       env: {
         ARCHIVARIUS_API_ENDPOINT: process.env.PRODUCTION
@@ -56,25 +31,21 @@ module.exports = configure(function (ctx) {
           : 'https://catalog.flipp.dev/api/v0',
         PRODUCTION: process.env.PRODUCTION
       },
-      // transpile: false,
-      // publicPath: '/',
 
-      // Add dependencies for transpiling with Babel (Array of string/regex)
-      // (from node_modules, which are by default not transpiled).
-      // Applies only if "transpile" is set to true.
-      // transpileDependencies: [],
+      beforeBuild: async ({ quasarConf }) => {
+        await compileProtofiles()
+        if (quasarConf.ctx.mode.electron) {
+          await moveWorkers()
+        }
+      },
 
-      // rtl: true, // https://quasar.dev/options/rtl-support
-      // preloadChunks: true,
-      // showProgress: false,
-      // gzip: true,
-      // analyze: true,
+      beforeDev: async ({ quasarConf }) => {
+        await compileProtofiles()
+        if (quasarConf.ctx.mode.electron) {
+          await moveWorkers()
+        }
+      },
 
-      // Options below are automatically set depending on the env, set them if you want to override
-      // extractCSS: false,
-
-      // https://quasar.dev/quasar-cli/handling-webpack
-      // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
       chainWebpack (chain) {
         chain.plugin('eslint-webpack-plugin')
           .use(ESLintPlugin, [{ extensions: ['js', 'vue'] }]),
@@ -85,52 +56,27 @@ module.exports = configure(function (ctx) {
       }
     },
 
-    // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       server: {
         type: 'http'
       },
       port: 8080,
-      open: false // opens browser window automatically
+      open: false
     },
 
-    // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
     framework: {
       config: {},
-
-      // iconSet: 'material-icons', // Quasar icon set
-      // lang: 'en-US', // Quasar language pack
-
-      // For special cases outside of where the auto-import strategy can have an impact
-      // (like functional components as one of the examples),
-      // you can manually specify Quasar components/directives to be available everywhere:
-      //
-      // components: [],
-      // directives: [],
-
-      // Quasar plugins
       plugins: [
         'Notify'
       ]
     },
 
-    // animations: 'all', // --- includes all animations
-    // https://quasar.dev/options/animations
     animations: [],
 
-    // https://quasar.dev/quasar-cli/developing-ssr/configuring-ssr
     ssr: {
       pwa: false,
-
-      // manualStoreHydration: true,
-      // manualPostHydrationTrigger: true,
-
-      prodPort: 3000, // The default port that the production server should use
-      // (gets superseded if process.env.PORT is specified at runtime)
-
+      prodPort: 3000,
       maxAge: 1000 * 60 * 60 * 24 * 30,
-      // Tell browser when a file from the server should expire from cache (in ms)
-
       chainWebpackWebserver (chain) {
         chain.plugin('eslint-webpack-plugin')
           .use(ESLintPlugin, [{ extensions: ['js'] }])
@@ -142,13 +88,10 @@ module.exports = configure(function (ctx) {
       ]
     },
 
-    // https://quasar.dev/quasar-cli/developing-pwa/configuring-pwa
     pwa: {
-      workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
-      workboxOptions: {}, // only for GenerateSW
+      workboxPluginMode: 'GenerateSW',
+      workboxOptions: {},
 
-      // for the custom service worker ONLY (/src-pwa/custom-service-worker.[js|ts])
-      // if using workbox in InjectManifest mode
       chainWebpackCustomSW (chain) {
         chain.plugin('eslint-webpack-plugin')
           .use(ESLintPlugin, [{ extensions: ['js'] }])
@@ -192,49 +135,46 @@ module.exports = configure(function (ctx) {
       }
     },
 
-    // Full list of options: https://quasar.dev/quasar-cli/developing-cordova-apps/configuring-cordova
     cordova: {
-      // noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
     },
 
-    // Full list of options: https://quasar.dev/quasar-cli/developing-capacitor-apps/configuring-capacitor
     capacitor: {
       hideSplashscreen: true
     },
 
-    // Full list of options: https://quasar.dev/quasar-cli/developing-electron-apps/configuring-electron
     electron: {
       bundler: 'builder',
 
       packager: {
-        // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-
-        // OS X / Mac App Store
-        // appBundleId: '',
-        // appCategoryType: '',
-        // osxSign: '',
-        // protocol: 'myapp://path',
-
-        // Windows only
-        // win32metadata: { ... }
       },
 
       builder: {
-        // https://www.electron.build/configuration/configuration
         appId: 'lab.flipper.net',
         publish: {
           provider: 'github',
           publishAutoUpdate: false
         },
+        /* files: [
+          {
+            from: "src/util/untar/",
+            to: "src-electron/extraResources/",
+            filter: [
+              "untar-worker.js"
+            ]
+          }
+        ], */
         extraResources: [
           {
-            "from": "src-electron/extraResources/",
-            "to": "extraResources",
-            "filter": [
+            from: "src-electron/extraResources/",
+            to: "extraResources",
+            filter: [
               "**/*"
             ]
           }
         ],
+        /* asarUnpack: [
+          "src/util/untar/untar-worker.js"
+        ], */
         mac: {
           target: {
             target: 'default',
@@ -244,13 +184,11 @@ module.exports = configure(function (ctx) {
         }
       },
 
-      // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
       chainWebpackMain (chain) {
         chain.plugin('eslint-webpack-plugin')
           .use(ESLintPlugin, [{ extensions: ['js'] }])
       },
 
-      // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
       chainWebpackPreload (chain) {
         chain.plugin('eslint-webpack-plugin')
           .use(ESLintPlugin, [{ extensions: ['js'] }])
