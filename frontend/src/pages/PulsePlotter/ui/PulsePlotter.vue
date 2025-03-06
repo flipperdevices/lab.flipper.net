ba55d02cbd6fd371b7ea95294e79cf9a64edd20e
Slipn3r <61992469+Slipn3r@users.noreply.github.com>
2025-03-06 03:46:59 +0300

    chore: Add description for plotter

--- frontend/src/pages/PulsePlotter/ui/PulsePlotter.vue
+++ frontend/src/pages/PulsePlotter/ui/PulsePlotter.vue
@@ -1,10 +1,36 @@
 <template>
   <GenericPageLayout
     title="Pulse Plotter"
-    icon="flipper:nfc"
+    icon="flipper:subtools"
     description="Raw Sub-GHz/Infrared/RFID pulse plotter"
   >
     <PulsePlotter />
+
+    <template #info>
+      <h6 class="q-mt-none q-mb-sm">About Pulse plotter</h6>
+      <p>
+        Sub-GHz/RFID/Infrared signal plotter, or Pulse plotter in short, is a
+        tool to visualize raw signals (aka pulses) from various sources. Aside from
+        visualizing saved signals, it can also be helpful to analyze raw
+        captures.
+      </p>
+      <p>
+        Accepted file formats are:
+        <ul class="q-mt-none q-pl-lg">
+          <li>Sub-GHz RAW captures (<b>.sub</b>)</li>
+          <li>RFID RAW captures (<b>.raw</b>)</li>
+          <li>Infrared signal/remote files (<b>.ir</b>)</li>
+          <li>other <b>Flipper File Format RAW</b>-compatible files</li>
+        </ul>
+      </p>
+      <p>After parsing the file, plotter will try to guess signal modulation. You can also use the slicer to figure it out manually.</p>
+      <p class="q-mb-none">
+        <a href="https://docs.flipper.net/sub-ghz/read-raw" target="_blank"
+          >Read more about capturing RAW signals</a
+        >
+        on Flipper Docs
+      </p>
+    </template>
   </GenericPageLayout>
 </template>
 
